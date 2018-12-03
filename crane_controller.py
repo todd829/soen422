4d7d31966200873b69ff3867e16db90899fbb6a3
todd829 <patodd829@gmail.com>
2018-12-03 17:20:26 -0500

    refactoring

--- crane_controller.py
+++ crane_controller.py
@@ -2,51 +2,140 @@ from pynput import keyboard
 import sys
 
 
-move = {
-    'up': False,
-    'left': False,
-    'down': False,
-    'right': False
-}
+class MoveStrategy:
+
+    def start(self):
+        pass
+    
+    def stop(self):
+        pass
+
+    def is_moving(self):
+        pass
 
 
-def on_press(key):
+class MoveUpStrategy(MoveStrategy):
 
-    key_press = {
-        keyboard.KeyCode.from_char('w'): 'up',
-        keyboard.KeyCode.from_char('a'): 'left',
-        keyboard.KeyCode.from_char('s'): 'down',
-        keyboard.KeyCode.from_char('d'): 'right'
-    }
+    def __init__(self):
+        self._is_moving = False
 
-    if key in key_press.keys():
-        if move[key_press[key]] == False:
-            move[key_press[key]] = True
-            print('press', move)
-
-
-def on_release(key):
-
-    if key == keyboard.Key.esc:
-        end()
-
-    key_release = {
-        keyboard.KeyCode.from_char('w'): 'up',
-        keyboard.KeyCode.from_char('a'): 'left',
-        keyboard.KeyCode.from_char('s'): 'down',
-        keyboard.KeyCode.from_char('d'): 'right'
-    }
-
-    if key in key_release.keys():
-        if move[key_release[key]] == True:
-            move[key_release[key]] = False
-            print('release', move)
+    def start(self):
+        self._is_moving = True
+        # TODO Add implementation
+        print("\nstart moving up\n");
+    
+    def stop(self):
+        self._is_moving = False
+        # TODO Add implementation
+        print("\nstop moving up\n");
+    
+    def is_moving(self):
+        return self._is_moving
 
 
-def end():
-    sys.exit(1)
+class MoveLeftStrategy(MoveStrategy):
+
+    def __init__(self):
+        self._is_moving = False
+
+    def start(self):
+        self._is_moving = True
+        # TODO Add implementation
+        print("\nstart moving left\n");
+    
+    def stop(self):
+        self._is_moving = False
+        # TODO Add implementation
+        print("\nstop moving left\n");
+    
+    def is_moving(self):
+        return self._is_moving
+
+
+class MoveDownStrategy(MoveStrategy):
+
+    def __init__(self):
+        self._is_moving = False
+
+    def start(self):
+        self._is_moving = True
+        # TODO Add implementation
+        print("\nstart moving down\n");
+    
+    def stop(self):
+        self._is_moving = False
+        # TODO Add implementation
+        print("\nstop moving down\n");
+    
+    def is_moving(self):
+        return self._is_moving
+
+
+class MoveRightStrategy(MoveStrategy):
+
+    def __init__(self):
+        self._is_moving = False
+
+    def start(self):
+        self._is_moving = True
+        # TODO Add implementation
+        print("\nstart moving right\n");
+    
+    def stop(self):
+        self._is_moving = False
+        # TODO Add implementation
+        print("\nstop moving right\n");
+    
+    def is_moving(self):
+        return self._is_moving
+        
+
+class MovementCommand:
+
+    def __init__(self):
+        self.move_up = MoveUpStrategy()
+        self.move_left = MoveLeftStrategy()
+        self.move_down = MoveDownStrategy()
+        self.move_right = MoveRightStrategy()
+
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
+            listener.join()
+
+    def on_press(self, key):
+
+        move_by_key_press = {
+            keyboard.KeyCode.from_char('w'): self.move_up,
+            keyboard.KeyCode.from_char('a'): self.move_left,
+            keyboard.KeyCode.from_char('s'): self.move_down,
+            keyboard.KeyCode.from_char('d'): self.move_right
+        }
+
+        if key in move_by_key_press.keys():
+            if move_by_key_press[key].is_moving() == False:
+                move_by_key_press[key].start()
+                # print('press', move)
+
+    def on_release(self, key):
+
+        if key == keyboard.Key.esc:
+            self.end()
+
+        move_by_key_release = {
+            keyboard.KeyCode.from_char('w'): self.move_up,
+            keyboard.KeyCode.from_char('a'): self.move_left,
+            keyboard.KeyCode.from_char('s'): self.move_down,
+            keyboard.KeyCode.from_char('d'): self.move_right
+        }
+
+        if key in move_by_key_release.keys():
+            if move_by_key_release[key].is_moving() == True:
+                move_by_key_release[key].stop()
+                # print('release', move)
+
+    def end(self):
+        sys.exit(1)
 
 
 if __name__ == '__main__':
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
+    MovementCommand()
+    
