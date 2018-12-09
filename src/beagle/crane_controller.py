2feac8ee6ab0d0d8b52b3843a7fd76526f5b2ff7
todd829 <patodd829@gmail.com>
2018-12-09 16:17:44 -0500

    crane controller send integer code via spi

--- src/beagle/crane_controller.py
+++ src/beagle/crane_controller.py
@@ -1,89 +1,118 @@
-from pynput import keyboard
+import pygame
 import sys
 import random
 import spidev
 import time
-        
+
 
 class CraneController:
 
     def __init__(self):
-        """Crane Controller responsible with key change and sending modification of movements via SPI"""
-        # spi setup
+        """
+        Crane Controller responsible with controller inputs and sending modification of movements via SPI
+        
+        Possible Movements are:
+
+        UP: 1
+        DOWN: 2
+        LEFT: 10
+        RIGHT: 20
+        HIGH: 100
+        LOW: 200
+
+        Possible Movement combinations are:
+
+        UP+LEFT: 11
+        UP+RIGHT: 21
+        DOWN+LEFT: 12
+        DOWN+RIGHT: 22
+
+        UP+HIGH: 101
+        UP+LOW: 201
+        DOWN+HIGH: 102
+        DOWN+LOW: 202
+        LEFT+HIGH: 110
+        LEFT+LOW: 210
+        RIGHT+HIGH: 120
+        RIGHT+LOW: 220
+
+        UP+LEFT+HIGH: 111
+        UP+LEFT+LOW: 211
+        UP+RIGHT+HIGH: 121
+        UP+RIGHT+LOW: 221
+        DOWN+LEFT+HIGH: 112
+        DOWN+LEFT+LOW: 212
+        DOWN+RIGHT+HIGH: 122
+        DOWN+RIGHT+LOW: 222
+        """
+        # SPI setup
         self.spi = spidev.SpiDev()
         self.spi.open(1, 0)
         self.spi.max_speed_hz = 90000
 
-        # Motor 1
-        self.movements = {
+        self.move = {
             # Motor 1
-            'move_up': 0,
-            'move_down': 0,
+            'UP': 0,
+            'DOWN': 0,
             # Motor 2
-            'move_left': 0,
-            'move_right': 0,
+            'LEFT': 0,
+            'RIGHT': 0,
             # Motor 3
-            'move_high': 0,
-            'move_low': 0
+            'HIGH': 0,
+            'LOW': 0
         }
 
-        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
-            listener.join()
+        pygame.init()
 
-    def on_press(self, key):
-        """On Press of a certain key, modify movements and send vie SPI"""
+        # Set the width and height of the screen [width,height]
+        size = [500, 500]
+        screen = pygame.display.set_mode(size)
 
-        move_by_key_press = {
-            keyboard.KeyCode.from_char('w'): self.movements['move_up'],
-            keyboard.KeyCode.from_char('a'): self.movements['move_left'],
-            keyboard.KeyCode.from_char('s'): self.movements['move_down'],
-            keyboard.KeyCode.from_char('d'): self.movements['move_right'],
-            keyboard.KeyCode.from_char('r'): self.movements['move_high'],
-            keyboard.KeyCode.from_char('f'): self.movements['move_low']
-        }
+        pygame.display.set_caption("Crane")
 
-        if key in move_by_key_press.keys():
-            if move_by_key_press[key] == 0:
-                move_by_key_press[key] = 1
-                self.send_spi_command()
+        # Used to manage how fast the screen updates
+        clock = pygame.time.Clock()
 
-    def on_release(self, key):
-        """On Release of a certain key, modify movements and send vie SPI"""
-
-        if key == keyboard.Key.esc:
-            self.end()
-
-        move_by_key_release = {
-            keyboard.KeyCode.from_char('w'): self.movements['move_up'],
-            keyboard.KeyCode.from_char('a'): self.movements['move_left'],
-            keyboard.KeyCode.from_char('s'): self.movements['move_down'],
-            keyboard.KeyCode.from_char('d'): self.movements['move_right'],
-            keyboard.KeyCode.from_char('r'): self.movements['move_high'],
-            keyboard.KeyCode.from_char('f'): self.movements['move_low']
-        }
-
-        if key in move_by_key_release.keys():
-            if move_by_key_release[key] == 1:
-                move_by_key_release[key] = 0
-                self.send_spi_command()
-    
-    def spi_command(self):
-        """Create SPI command to send in the form [0, 0, 0, 0, 0, 0] where each group of 2 are a motor's drive/reverse"""
-        return [
-            self.movements['move_up'], self.movements['move_down'],
-            self.movements['move_left'], self.movements['move_right'],
-            self.movements['move_high'], self.movements['move_low']
-            ]
-
-    def send_spi_command(self):
-        """Send SPI command to the Arduino"""
-        command = self.spi_command()
-        print(self.spi.xfer(command))
-        time.sleep(1)
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
         
-    def end(self):
-        """End the app on ESC"""
-        sys.exit(1)
+        # START READING INPUTS FROM USER
+
+        while True:
+
+            for event in pygame.event.get():
+                event_d = event.__dict__
+
+                if event.type == pygame.JOYAXISMOTION:  # ARROWS USED
+
+                    if event_d['axis'] == 1:  # ARROWS UP/DOWN PRESSED
+                        self.move['UP'] = 1 if event_d['value'] < -1 else 0
+                        self.move['DOWN'] = 2 if event_d['value'] == 1 else 0
+
+                    if event_d['axis'] == 0:  # ARROWS LEFT/RIGHT PRESSED
+                        self.move['LEFT'] = 10 if event_d['value'] < -1 else 0
+                        self.move['RIGHT'] = 20 if event_d['value'] == 1 else 0
+
+                if event.type == pygame.JOYBUTTONDOWN:  # A or B BUTTON PRESSED
+                    self.move['HIGH'] = 100 if event_d['button'] == 1 else 0
+                    self.move['LOW'] = 200 if event_d['button'] == 0 else 0
+
+                if event.type == pygame.JOYBUTTONUP:  # A or B BUTTON RELEASED
+                    self.move['HIGH'] = 0 if event_d['button'] == 1 else 0
+                    self.move['LOW'] = 0 if event_d['button'] == 0 else 0
+
+                # For debugging
+                # print(self.move)
+                self.send_spi()
+    
+    def send_spi(self):
+        """Send SPI command to the Arduino"""
+        message = sum([self.move[key] for key in self.move.keys()])
+
+        # For debugging
+        # print(message)
+
+        print(self.spi.xfer(message))
 
 
 if __name__ == '__main__':
